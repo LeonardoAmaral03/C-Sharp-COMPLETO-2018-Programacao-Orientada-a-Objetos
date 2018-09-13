d6e5478767cd28d19a2073b2fd82c43cc404e0db
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-12 21:45:23 -0300

    Apresentação da linguagem C# e tópicos básicos - Estrutura e cabeçalhos de um programa C#

---
+++ primeiro-projeto/primeiro-projeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Olá Mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
