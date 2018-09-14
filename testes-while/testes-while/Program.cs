3131f451be37f654ee547f376e44c5fc9d2e27f2
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-14 19:41:48 -0300

    Apresentação da linguagem C# e tópicos básicos - Estrutura repetitiva enquanto (while) em C#

---
+++ testes-while/testes-while/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace testes_while {
+    class Program {
+        static void Main(string[] args) {
+            int idade, soma, cont;
+            double media;
+
+            soma = 0;
+            cont = 0;
+
+            idade = int.Parse(Console.ReadLine());
+
+            while (idade >= 0) {
+                soma += idade;
+                cont++;
+
+                idade = int.Parse(Console.ReadLine());
+            }
+
+            if (cont == 0) {
+                Console.WriteLine("IMPOSSIVEL CALCULAR!");
+            }
+            else {
+                media = (double)soma / cont;
+
+                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
