2fca29af9b8e3eedf6ec16b9eeccd22393c55efa
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-14 20:06:33 -0300

    Apresentação da linguagem C# e tópicos básicos - Estrutura repetitiva para (for) em C#

---
+++ testes-for/testes-for/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace testes_for {
+    class Program {
+        static void Main(string[] args) {
+            int N;
+            double x, y, resultado;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++) {
+                string[] vet = Console.ReadLine().Split(' ');
+
+                x = double.Parse(vet[0], CultureInfo.InvariantCulture);
+                y = double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+                if (y == 0.0) {
+                    Console.WriteLine("nao existe");
+                }
+                else {
+                    resultado = x / y;
+
+                    Console.WriteLine(resultado.ToString("F3", CultureInfo.InvariantCulture));
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
