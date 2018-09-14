8748e3a4ec52b44990c6183ecd338c08265343e0
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-14 18:27:23 -0300

    Apresentação da linguagem C# e tópicos básicos - Como fazer entrada de dados em C#

---
+++ testes-input/testes-input/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+
+            string x;
+            int y;
+            double z;
+            char w;
+
+            x = Console.ReadLine();
+            y = int.Parse(Console.ReadLine());
+            z = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            w = char.Parse(Console.ReadLine());
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(w);
+
+            //Console.ReadLine();
+
+            // Para ler vários dados na mesma linha
+
+            string a;
+            int b;
+            double c;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = vet[0];
+            b = int.Parse(vet[1]);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+        }
+    }
+}
