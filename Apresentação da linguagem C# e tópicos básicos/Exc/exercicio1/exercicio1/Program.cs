0eb074bd86fa3efa1d4c1f3c25a1297a27eff322
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-14 20:53:11 -0300

    Apresentação da linguagem C# e tópicos básicos - Exercício 1

---
+++ Apresentação da linguagem C# e tópicos básicos/Exc/exercicio1/exercicio1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio1 {
+    class Program {
+        static void Main(string[] args) {
+
+            int volInicial, vazBomba, tempo, volFinal;
+
+            volInicial = int.Parse(Console.ReadLine());
+            vazBomba = int.Parse(Console.ReadLine());
+            tempo = int.Parse(Console.ReadLine());
+
+            volFinal = volInicial - vazBomba * tempo;
+
+            if (volFinal < 0) {
+                Console.WriteLine("nao eh possivel");
+            }
+            else {
+                Console.WriteLine(volFinal);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
