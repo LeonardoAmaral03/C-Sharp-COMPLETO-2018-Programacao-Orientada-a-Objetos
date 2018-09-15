2ac032f32e8334686d8242bdfae3dc7b8f48018c
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-14 22:53:03 -0300

    Apresentação da linguagem C# e tópicos básicos - Exercício 4

---
+++ Apresentação da linguagem C# e tópicos básicos/Exc/exercicio4/exercicio4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio4 {
+    class Program {
+        static void Main(string[] args) {
+
+            double valGlicose;
+
+            valGlicose = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (valGlicose <= 100.0) {
+                Console.WriteLine("Normal");
+            }
+            else {
+                if (valGlicose > 100 && valGlicose <= 140) {
+                    Console.WriteLine("Elevado");
+                }
+                else {
+                    Console.WriteLine("Diabetes");
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
