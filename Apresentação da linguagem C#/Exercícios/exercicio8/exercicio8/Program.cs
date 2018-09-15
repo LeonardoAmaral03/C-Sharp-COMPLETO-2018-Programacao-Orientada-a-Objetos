fe13f7c738eaf41626841cb40e6dd2c419629dd4
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-15 19:43:43 -0300

    Apresentação da linguagem C# e tópicos básicos - Exercício 8

---
+++ Apresentação da linguagem C#/Exercícios/exercicio8/exercicio8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio8 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N, A, B;
+
+            N = int.Parse(Console.ReadLine());
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            for (int i = A; i <= B; i++) {
+                if (i % N == 0) {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
