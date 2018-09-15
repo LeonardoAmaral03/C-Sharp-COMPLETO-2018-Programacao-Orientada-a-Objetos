6aa13d17a162f5c768f591d57213fbf55eaf5538
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-14 22:36:43 -0300

    Apresentação da linguagem C# e tópicos básicos - Exercício 3

---
+++ Apresentação da linguagem C# e tópicos básicos/Exc/exercicio3/exercicio3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exercicio3 {
+    class Program {
+        static void Main(string[] args) {
+
+            int largFoto, altFoto, largPortRetrato, altPortRetrato;
+
+            string[] vetFoto = Console.ReadLine().Split(' ');
+            largFoto = int.Parse(vetFoto[0]);
+            altFoto = int.Parse(vetFoto[1]);
+
+            string[] vetPortRetrato = Console.ReadLine().Split(' ');
+            largPortRetrato = int.Parse(vetPortRetrato[0]);
+            altPortRetrato = int.Parse(vetPortRetrato[1]);
+
+            if (largFoto <= largPortRetrato && altFoto <= altPortRetrato) {
+                Console.WriteLine("SIM");
+            }
+            else {
+                if (largFoto <= altPortRetrato && altFoto <= largPortRetrato) {
+                    Console.WriteLine("SIM");
+                }
+                else {
+                    Console.WriteLine("NAO");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
