5f9be89fe3eee7e09a82df995f5e84411d091a03
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-15 20:14:34 -0300

    Apresentação da linguagem C# e tópicos básicos - Exercício 10

---
+++ Apresentação da linguagem C#/Exercícios/exercicio10/exercicio10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio10 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N;
+            double n1, n2, n3, nFinal;
+            string[] vet;
+
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++) {
+                vet = Console.ReadLine().Split(' ');
+
+                n1 = double.Parse(vet[0], CultureInfo.InvariantCulture);
+                n2 = double.Parse(vet[1], CultureInfo.InvariantCulture);
+                n3 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+                nFinal = n1 + n2 + n3;
+
+                Console.WriteLine(nFinal.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
