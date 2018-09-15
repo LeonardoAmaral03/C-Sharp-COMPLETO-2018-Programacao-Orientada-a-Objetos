4e313e5ceaca8ceb70e0f1f1da9bd18ee57c799c
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-15 20:01:45 -0300

    Apresentação da linguagem C# e tópicos básicos - Exercício 9

---
+++ Apresentação da linguagem C#/Exercícios/exercicio9/exercicio9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio9 {
+    class Program {
+        static void Main(string[] args) {
+
+            double tempoTotal, tempoJogando, tempoRestante;
+            int N;
+
+            tempoTotal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            N = int.Parse(Console.ReadLine());
+
+            tempoRestante = tempoTotal;
+
+            for (int i = 0; i < N; i++) {
+                tempoJogando = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                tempoRestante -= tempoJogando;
+
+                if (tempoRestante < 0) {
+                    Console.WriteLine("recarregar");
+                }
+                else {
+                    Console.WriteLine(tempoRestante.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
