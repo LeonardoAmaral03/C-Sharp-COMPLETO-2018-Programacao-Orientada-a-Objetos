f6568e3690d3eb61af8440eb21b2e89d71980eb0
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-15 19:35:29 -0300

    Apresentação da linguagem C# e tópicos básicos - Exercício 7

---
+++ Apresentação da linguagem C#/Exercícios/exercicio7/exercicio7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio7 {
+    class Program {
+        static void Main(string[] args) {
+
+            int num;
+
+            num = int.Parse(Console.ReadLine());
+
+            while (num != 0) {
+
+                Console.WriteLine(num * num);
+
+                num = int.Parse(Console.ReadLine());
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
