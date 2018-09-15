e1c4117827457f362948182bae117a609655e712
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-14 21:50:14 -0300

    Apresentação da linguagem C# e tópicos básicos - Exercício 2

---
+++ Apresentação da linguagem C# e tópicos básicos/Exc/exercicio2/exercicio2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio2 {
+    class Program {
+        static void Main(string[] args) {
+
+            double n1, n2, n3, nFinal;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            n1 = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            n2 = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            n3 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            nFinal = n1 + n2 + n3;
+
+            if(nFinal < 60.00) {
+                Console.WriteLine(nFinal.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("DEPENDENCIA");
+            }
+            else {
+                Console.WriteLine(nFinal.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
