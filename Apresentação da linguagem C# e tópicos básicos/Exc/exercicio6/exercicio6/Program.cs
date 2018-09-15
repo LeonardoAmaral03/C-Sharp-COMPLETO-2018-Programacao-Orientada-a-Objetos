f40743a2246946124ae2dff00b4995e892ad28c0
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-15 19:21:52 -0300

    Apresentação da linguagem C# e tópicos básicos - Exercício 6

---
+++ Apresentação da linguagem C# e tópicos básicos/Exc/exercicio6/exercicio6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio6 {
+    class Program {
+        static void Main(string[] args) {
+
+            int n1, n2, somaImpares;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            n1 = int.Parse(vet[0]);
+            n2 = int.Parse(vet[1]);
+
+            somaImpares = 0;
+
+            for (int i = n1; i <= n2; i++) {
+                if (i % 2 != 0) {
+                    somaImpares += i;
+                }
+            }
+
+            Console.WriteLine(somaImpares);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
