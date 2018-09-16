4b54ff7b263becc230a694445da3656cdc309b3f
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-16 18:47:40 -0300

    Introdução à Programação Orientada a Objetos - Exercício 1

---
+++ Introdução à Programação Orientada a Objetos/Exercícios/ex1/ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex1 {
+    class Program {
+        static void Main(string[] args) {
+
+            Retangulo ret;
+            ret = new Retangulo();
+
+            ret.largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(ret);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Introdução à Programação Orientada a Objetos/Exercícios/ex1/ex1/Retangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex1 {
+    class Retangulo {
+
+        public double largura, altura;
+
+        public double Area() {
+            return largura * altura;
+        }
+
+        public double Perimetro() {
+            return largura * 2 + altura * 2;
+        }
+
+        public double Diagonal() {
+            return Math.Sqrt((Math.Pow(largura, 2) + Math.Pow(altura, 2)));
+        }
+
+        public override string ToString() {
+            return "AREA = " + Area().ToString("F2", CultureInfo.InvariantCulture) + "\n"
+                + "PERIMETRO = " + Perimetro().ToString("F2", CultureInfo.InvariantCulture) + "\n"
+                + "DIAGONAL = " + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
