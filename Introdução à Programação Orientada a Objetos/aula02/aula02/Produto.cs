12dd8b256f26fc80f4785358c9f0c7e5729d550e
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-16 17:10:18 -0300

    Introdução à Programação Orientada a Objetos - Implementando os métodos da classe Produto

--- Introdução à Programação Orientada a Objetos/aula02/aula02/Produto.cs
+++ Introdução à Programação Orientada a Objetos/aula02/aula02/Produto.cs
@@ -7,6 +7,18 @@ namespace curso {
         public double preco;
         public int quantidadeEmEstoque;
 
+        public double ValorTotalEmEstoque() {
+            return preco * quantidadeEmEstoque;
+        }
+
+        public void RealizarEntrada(int quantidade) {
+            quantidadeEmEstoque += quantidade;
+        }
+
+        public void RealizarSaida(int quantidade) {
+            quantidadeEmEstoque -= quantidade;
+        }
+
         // método padrão da linguagem C# para converter um objeto para string
         public override string ToString() {
             return nome
