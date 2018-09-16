177e9ddb7a40b9ca1f16e9cbdb1554a1fdaf448c
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-16 19:21:45 -0300

    Introdução à Programação Orientada a Objetos - Exercício 2

---
+++ Introdução à Programação Orientada a Objetos/Exercícios/ex2/ex2/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace ex2 {
+    class Funcionario {
+
+        public string nome;
+        public double salarioBruto, desconto;
+
+        public double SalarioLiquido() {
+            return salarioBruto - desconto;
+        }
+
+        public void AumentarSalario(double porcentagem) {
+            salarioBruto += salarioBruto * porcentagem / 100;
+        }
+
+        public override string ToString() {
+            return "Dados do funcionário: " 
+                + nome
+                + ", R$ "
+                + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Introdução à Programação Orientada a Objetos/Exercícios/ex2/ex2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex2 {
+    class Program {
+        static void Main(string[] args) {
+
+            double porcentagem;
+            Funcionario func;
+            func = new Funcionario();
+
+            Console.Write("Nome: ");
+            func.nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            func.salarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Desconto: ");
+            func.desconto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine(func);
+            Console.WriteLine();
+
+            Console.Write("Deseja aumentar o salário em qual porcentagem? ");
+            porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            func.AumentarSalario(porcentagem);
+
+            Console.WriteLine();
+            Console.WriteLine(func);
+
+            Console.ReadLine();
+
+        }
+    }
+}
