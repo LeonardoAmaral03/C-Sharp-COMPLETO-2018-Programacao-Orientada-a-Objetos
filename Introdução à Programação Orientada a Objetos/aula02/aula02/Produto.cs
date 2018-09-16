7fd07151f402504ce70535d93d3aa0b4236f532d
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-16 16:38:44 -0300

    Introdução à Programação Orientada a Objetos - Começando a resolver um segundo problema exemplo

---
+++ Introdução à Programação Orientada a Objetos/aula02/aula02/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace curso {
+    class Produto {
+
+        public string nome;
+        public double preco;
+        public int quantidadeEmEstoque;
+
+
+
+    }
+}
---
+++ Introdução à Programação Orientada a Objetos/aula02/aula02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto P;
+            P = new Produto();
+
+            Console.WriteLine("Digite os dados do produto:");
+            Console.Write("Nome: ");
+            P.nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            P.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            P.quantidadeEmEstoque = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Você digitou: " + P.nome + ", " + 
+                P.preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + P.quantidadeEmEstoque);
+
+            Console.ReadLine();
+
+        }
+    }
+}
