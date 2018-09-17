3335f23493413f1f466aa174edafec1aa2c68a73
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-17 17:57:18 -0300

    Introdução à Programação Orientada a Objetos - Exercício 3

---
+++ Introdução à Programação Orientada a Objetos/Exercícios/ex3/ex3/Aluno.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex3 {
+    class Aluno {
+
+        public string nome;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+
+        public double NotaFinal() {
+            return nota1 + nota2 + nota3;
+        }
+
+        public bool Aprovado() {
+            if(NotaFinal() < 60.00) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+
+        public double PontosFaltantes() {
+            return 60.00 - NotaFinal();
+        }
+
+        public override string ToString() {
+            string saida;
+
+            saida = "NOTA FINAL = " + NotaFinal().ToString("F2", CultureInfo.InvariantCulture) + "\n";
+
+            if (Aprovado()) {
+                return saida
+                    + "APROVADO";
+            }
+            else {
+                return saida
+                    + "REPROVADO" + "\n"
+                    + "FALTARAM " + PontosFaltantes().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS";
+            }
+        }
+
+    }
+}
---
+++ Introdução à Programação Orientada a Objetos/Exercícios/ex3/ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex3 {
+    class Program {
+        static void Main(string[] args) {
+
+            Aluno aluno;
+            aluno = new Aluno();
+
+            aluno.nome = Console.ReadLine();
+            aluno.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(aluno);
+
+            Console.ReadLine();
+
+        }
+    }
+}
