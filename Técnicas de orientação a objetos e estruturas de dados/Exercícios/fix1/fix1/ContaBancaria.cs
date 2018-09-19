61cd1e2dae1cd33ddfac683e9c43489d4f12da69
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-18 21:44:35 -0300

    Técnicas de orientação a objetos e estruturas de dados - Exercício de fixação 1

---
+++ Técnicas de orientação a objetos e estruturas de dados/Exercícios/fix1/fix1/ContaBancaria.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace fix1 {
+    class ContaBancaria {
+
+        public string NumeroConta { get; private set; }
+        public string NomeTitular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(string numeroConta, string nomeTitular, double depositoInicial) {
+            this.NumeroConta = numeroConta;
+            this.NomeTitular = nomeTitular;
+            this.Saldo = depositoInicial;
+        }
+
+        public ContaBancaria(string numeroConta, string nomeTitular) {
+            this.NumeroConta = numeroConta;
+            this.NomeTitular = nomeTitular;
+            this.Saldo = 0;
+        }
+
+        public void Deposito(double deposito) {
+            this.Saldo += deposito;
+        }
+
+        public void Saque(double saque) {
+            this.Saldo -= (saque + 5.00);
+        }
+
+        public override string ToString() {
+            return "Conta: "
+                + this.NumeroConta
+                + ", Titular: "
+                + this.NomeTitular
+                + ", Saldo: $ "
+                + this.Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Técnicas de orientação a objetos e estruturas de dados/Exercícios/fix1/fix1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace fix1 {
+    class Program {
+        static void Main(string[] args) {
+
+            ContaBancaria conta;
+            string numConta, nomeTitular, resp;
+            double depositoInicial, deposito, saque;
+
+            Console.Write("Digite o número da conta: ");
+            numConta = Console.ReadLine();
+            Console.Write("Digite o nome do titular da conta: ");
+            nomeTitular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            resp = Console.ReadLine();
+
+            if (resp == "s") {
+                Console.Write("Digite o valor do depósito inicial: ");
+                depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                conta = new ContaBancaria(numConta, nomeTitular, depositoInicial);
+            }
+            else {
+                conta = new ContaBancaria(numConta, nomeTitular);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Conta criada:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+
+            Console.Write("Digite um valor para depósito: ");
+            deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(deposito);
+
+            Console.WriteLine("Conta atualizada:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+
+            Console.Write("Digite um valor para saque: ");
+            saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+
+            Console.WriteLine("Conta atualizada:");
+            Console.WriteLine(conta);
+
+            Console.ReadLine();
+
+        }
+    }
+}
