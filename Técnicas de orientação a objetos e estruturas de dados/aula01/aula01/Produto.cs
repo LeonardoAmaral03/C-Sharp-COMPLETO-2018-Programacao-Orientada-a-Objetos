8d244e09e1c80f6d91680c27110fac4b32181532
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-19 19:58:12 -0300

    Técnicas de orientação a objetos e estruturas de dados - Tipos referência e tipos valor

---
+++ Técnicas de orientação a objetos e estruturas de dados/aula01/aula01/Produto.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+
+namespace curso {
+    class Produto {
+
+        public string Nome { get; private set; }
+        public double Preco { get; private set; }
+        public int QuantidadeEmEstoque { get; private set; }
+
+        //Construtor
+        public Produto(string nome, double preco, int quantidadeEmEstoque) {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.QuantidadeEmEstoque = quantidadeEmEstoque;
+        }
+
+        //Sobrecarga - permite definir mais de uma versão da mesma operação, diferencinado-as pela lista de parâmetros
+        public Produto(string nome, double preco) {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.QuantidadeEmEstoque = 0;
+        }
+
+        public double ValorTotalEmEstoque() {
+            return Preco * QuantidadeEmEstoque;
+        }
+
+        public void RealizarEntrada(int quantidade) {
+            QuantidadeEmEstoque += quantidade;
+        }
+
+        public void RealizarSaida(int quantidade) {
+            QuantidadeEmEstoque -= quantidade;
+        }
+
+        // método padrão da linguagem C# para converter um objeto para string
+        public override string ToString() {
+            return Nome
+                + ", R$ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + QuantidadeEmEstoque
+                + " unidades, Total: R$ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Técnicas de orientação a objetos e estruturas de dados/aula01/aula01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+               
+            // Classes são tipos referência
+            //Produto P1, P2;
+            //P1 = new Produto("TV", 1000.00, 0);
+            //P2 = P1;
+            //P2.RealizarEntrada(5);
+            //Console.WriteLine(P1.QuantidadeEmEstoque);
+
+            // Struct é um tipo valor, logo não precisa ser instanciado
+            Ponto A, B;
+
+            A.x = 10;
+            A.y = 20;
+
+            B = A;
+
+            B.y = 30;
+
+            Console.WriteLine(A);
+
+            Console.ReadLine();
+
+        }
+    }
+}
