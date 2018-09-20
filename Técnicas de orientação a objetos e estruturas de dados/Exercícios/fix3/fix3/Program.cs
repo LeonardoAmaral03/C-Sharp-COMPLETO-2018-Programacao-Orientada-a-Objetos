fa3e0a27ec153210883f413b3d38ee2d2acfe381
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-20 20:29:23 -0300

    Técnicas de orientação a objetos e estruturas de dados - Exercício de fixação 3

---
+++ Técnicas de orientação a objetos e estruturas de dados/Exercícios/fix3/fix3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace fix3 {
+    class Program {
+        static void Main(string[] args) {
+
+            int M, N, X, l = 0, c = 0;
+            int[,] mat;
+
+            string[] MN = Console.ReadLine().Split(' ');
+
+            M = int.Parse(MN[0]);
+            N = int.Parse(MN[1]);
+
+            mat = new int[M, N];
+
+            for (int i = 0; i < M; i++) {
+                string[] linha = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++) {
+                    mat[i, j] = int.Parse(linha[j]);
+                }
+            }
+
+            X = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (mat[i, j] == X) {
+                        l = i;
+                        c = j;
+                    }
+                }
+            }
+
+            if (c != 0) {
+                Console.WriteLine("ESQUERDA: " + mat[l, c - 1]);
+            }
+
+            if (c != N-1) {
+                Console.WriteLine("DIREITA: " + mat[l, c + 1]);
+            }
+
+            if (l != 0) {
+                Console.WriteLine("ACIMA: " + mat[l - 1, c]);
+            }
+
+            if (l != M - 1) {
+                Console.WriteLine("ABAIXO: " + mat[l + 1, c]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
