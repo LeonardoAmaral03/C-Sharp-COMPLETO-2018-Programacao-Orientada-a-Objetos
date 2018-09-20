1c1c67c9baaf72579877af71762920ee27afc4f7
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-20 19:14:39 -0300

    Técnicas de orientação a objetos e estruturas de dados - Exercício de fixação 2

---
+++ Técnicas de orientação a objetos e estruturas de dados/Exercícios/fix2/fix2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fix2 {
+    class Program {
+        static void Main(string[] args) {
+
+            int alugueis, quarto;
+            string nome, email;
+            Estudante[] vetQuartos = new Estudante[10];
+
+            for (int i = 0; i < 10; i++) {
+                vetQuartos[i] = null;
+            }
+
+            Console.Write("Quantos aluguéis serão registrados? ");
+            alugueis = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int aluguel = 1; aluguel <= alugueis; aluguel++) {
+                Console.WriteLine("Dados do " + aluguel + "° aluguel:");
+
+                Console.Write("Nome: ");
+                nome = Console.ReadLine();
+                Console.Write("Email: ");
+                email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                quarto = int.Parse(Console.ReadLine());
+
+                vetQuartos[quarto] = new Estudante(nome, email);
+
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < 10; i++) {
+                if (vetQuartos[i] != null) {
+                    Console.WriteLine(i + ": " + vetQuartos[i]);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
