f3b03b5ca638b6af74fc0362f6f28eeacf222ffc
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-21 18:25:57 -0300

    Técnicas de orientação a objetos e estruturas de dados - Exercício de fixação 4

---
+++ Técnicas de orientação a objetos e estruturas de dados/Exercícios/fix4/fix4/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace fix4 {
+    class Funcionario {
+
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(string cpf, string nome, double salario) {
+            Cpf = cpf;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentoSalario(double porcentagem) {
+            Salario += Salario * (porcentagem / 100);
+        }
+
+        public override string ToString() {
+            return Cpf + ", " + Nome + ", " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Técnicas de orientação a objetos e estruturas de dados/Exercícios/fix4/fix4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace fix4 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N, pos;
+            string cpf, nome;
+            double salario, porcentagem;
+            List<Funcionario> listaFuncionarios = new List<Funcionario>();
+
+            Console.Write("Quantos funcionários serão cadastrados? ");
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++) {
+                Console.WriteLine("Dados do " + i + " ° funcionário:");
+                Console.Write("CPF: ");
+                cpf = Console.ReadLine();
+                Console.Write("Nome: ");
+                nome = Console.ReadLine();
+                Console.Write("Salario: ");
+                salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                listaFuncionarios.Add(new Funcionario(cpf, nome, salario));
+
+                Console.WriteLine();
+            }
+
+            Console.Write("Digite o CPF do funcionário que terá aumento: ");
+            cpf = Console.ReadLine();
+
+            pos = listaFuncionarios.FindIndex(x => x.Cpf == cpf);
+
+            if (pos != -1) {
+                Console.Write("Digite a porcentagem de aumento: ");
+                porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                listaFuncionarios[pos].AumentoSalario(porcentagem);
+            }
+            else {
+                Console.WriteLine("CPF INEXISTENTE");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Listagem atualizada de funcionários:");
+
+            for (int i = 0; i < N; i++) {
+                Console.WriteLine(listaFuncionarios[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
