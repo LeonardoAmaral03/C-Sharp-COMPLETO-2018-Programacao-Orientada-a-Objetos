4410e9aff0d4095837379107f8b2a679783a2c9d
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-21 17:40:22 -0300

    Técnicas de orientação a objetos e estruturas de dados - Listas

---
+++ Técnicas de orientação a objetos e estruturas de dados/aula04/aula04/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+
+            /*
+            List<string> lista = new List<string>();
+            lista.Add("João");
+            lista.Add("Maria");
+            lista.Add("Ana");
+            lista.Add("José");
+            //lista.Remove("Maria"); remove pelo objeto
+            //lista.RemoveAt(0); remove pelo index
+            int pos = lista.FindIndex(x => x == "Maria");
+            Console.WriteLine("Posição da Maria = " + pos);
+            for (int i = 0; i < lista.Count; i++) {
+                Console.WriteLine(lista[i]);
+            }
+            */
+
+            List<Produto> lista = new List<Produto>();
+
+            lista.Add(new Produto("TV", 900.00));
+            lista.Add(new Produto("Aspirador", 400.00));
+            lista.Add(new Produto("Geladeira", 2000.00));
+
+            //int pos = lista.FindIndex(x => x.Nome == "Aspirador");
+            //int pos = lista.FindIndex(x => x.Preco > 1000.00); so encontra a primeira ocorrencia
+            //int pos = lista.FindIndex(x => x.Preco < 1000.00);
+            int pos = lista.FindIndex(x => x.Preco > 3000.00); // ira retornar -1 pois não encontra ninguem
+            Console.WriteLine("Posição encontrada = " + pos);
+
+            for (int i = 0; i < lista.Count; i++) {
+                Console.WriteLine(lista[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
