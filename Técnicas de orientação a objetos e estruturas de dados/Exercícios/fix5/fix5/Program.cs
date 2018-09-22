d8ad7e07358ba43af7f4ba2fe3006a597bf0c233
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-21 23:08:07 -0300

    Técnicas de orientação a objetos e estruturas de dados - Exercício de fixação 5

---
+++ Técnicas de orientação a objetos e estruturas de dados/Exercícios/fix5/fix5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fix5 {
+    class Program {
+        static void Main(string[] args) {
+
+            int qtd, aluno;
+            char[] cursos = new char[] { 'A', 'B', 'C' };
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+
+            for (int i = 0; i < 3; i++) {
+                Console.Write("O curso " + cursos[i] + " possui quantos alunos? ");
+                qtd = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite os códigos dos alunos do curso " + cursos[i] + ":");
+
+                for (int j = 0; j < qtd; j++) {
+                    aluno = int.Parse(Console.ReadLine());
+                    if (i == 0) {
+                        A.Add(aluno);
+                    }
+                    else if (i == 1) {
+                        B.Add(aluno);
+                    }
+                    else {
+                        C.Add(aluno);
+                    }
+                }
+            }
+
+            B.UnionWith(C);
+            A.UnionWith(B);
+
+            Console.Write("Total de alunos: " + A.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
