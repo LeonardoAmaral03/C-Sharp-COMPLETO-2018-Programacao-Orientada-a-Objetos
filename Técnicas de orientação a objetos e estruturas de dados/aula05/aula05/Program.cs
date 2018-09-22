f48435ab81c0cce34c47ffb0774e76b24fec3f4e
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-21 22:40:04 -0300

    Técnicas de orientação a objetos e estruturas de dados - Conjuntos

---
+++ Técnicas de orientação a objetos e estruturas de dados/aula05/aula05/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aula05 {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+
+            A.Add(3);
+            A.Add(5);
+            A.Add(8);
+            A.Add(9);
+
+            B.Add(3);
+            B.Add(4);
+            B.Add(5);
+
+            //B.Remove(4);
+
+            //Diferença entre conjuntos (A - B)
+            //A.ExceptWith(B);
+
+            //União entre conjuntos (A u B)
+            //A.UnionWith(B);
+
+            //Intersecção entre conjuntos (A ∩ B)
+            A.IntersectWith(B);
+
+            foreach (int x in A) {
+                Console.WriteLine(x);
+            }
+
+            /*
+            Console.WriteLine("Digite um valor inteiro: ");
+            int N = int.Parse(Console.ReadLine());
+
+            if (B.Contains(N)) {
+                Console.WriteLine(N + " pertence ao conjunto B");
+            }
+            else {
+                Console.WriteLine(N + " não pertence ao conjunto B");
+            }
+            */
+
+            Console.ReadLine();
+        }
+    }
+}
