1bb5fbb0d06c48f496ac8ae3838e18f1d5bd3e39
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-09-24 19:13:20 -0300

    Técnicas de orientação a objetos e estruturas de dados - Solução do exercício resolvido - PARTE 1/3

---
+++ Técnicas de orientação a objetos e estruturas de dados/curso/curso/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using curso.dominio;
+
+namespace curso {
+    class Program {
+
+        public static List<Produto> produtos = new List<Produto>();
+
+        static void Main(string[] args) {
+
+            int opcao = 0;
+
+            produtos.Add(new Produto(1001, "Cadeira simples", 500.00));
+            produtos.Add(new Produto(1002, "Cadeira acolchoada", 900.00));
+            produtos.Add(new Produto(1003, "Sofá de três lugares", 2000.00));
+            produtos.Add(new Produto(1004, "Mesa retangular", 1500.00));
+            produtos.Add(new Produto(1005, "Mesa retangular", 2000.00));
+            produtos.Sort();
+
+            while (opcao != 5) {
+                Console.Clear();
+                Tela.MostrarMenu();
+
+                try {
+                    opcao = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Erro inesperado: " + e.Message);
+                    opcao = 0;
+                }
+
+                Console.WriteLine();
+
+                switch (opcao) {
+                    case 1:
+                        Tela.MostrarProdutos();
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+                        Console.WriteLine("Fim do programa!");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Técnicas de orientação a objetos e estruturas de dados/curso/curso/Tela.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso {
+    class Tela {
+
+        // Classe responsável por conter operações que interagem com o
+        // usuário no modo console
+
+        public static void MostrarMenu() {
+            Console.WriteLine("1 - Listar produtos ordenadamente");
+            Console.WriteLine("2 - Cadastrar produto");
+            Console.WriteLine("3 - Cadastrar pedido");
+            Console.WriteLine("4 - Mostrar dados de um pedido");
+            Console.WriteLine("5 - Sair");
+            Console.Write("Digite uma opção: ");
+        }
+
+        public static void MostrarProdutos() {
+            Console.WriteLine("LISTAGEM DE RPODUTOS:");
+
+            for (int i = 0; i < Program.produtos.Count; i++) {
+                Console.WriteLine(Program.produtos[i]);
+            }
+        }
+
+    }
+}
