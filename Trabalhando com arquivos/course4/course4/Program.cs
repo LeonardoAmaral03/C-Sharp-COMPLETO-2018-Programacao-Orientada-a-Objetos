84324b96c7c05921e48f43a00c69b8139b0400d9
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-10-02 20:10:22 -0300

    Trabalhando com arquivos - StreamWriter

---
+++ Trabalhando com arquivos/course4/course4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace course4 {
+    class Program {
+        static void Main(string[] args) {
+
+            string sourcePath = @"C:\Users\Leonardo\Documents\Leonardo\Git\C#\C# Completo 2018 - Programação Orientada a Objetos\Trabalhando com arquivos\file1.txt";
+            string targetPath = @"C:\Users\Leonardo\Documents\Leonardo\Git\C#\C# Completo 2018 - Programação Orientada a Objetos\Trabalhando com arquivos\file2.txt";
+
+            try {
+
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath)) {
+                    foreach (string line in lines) {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
