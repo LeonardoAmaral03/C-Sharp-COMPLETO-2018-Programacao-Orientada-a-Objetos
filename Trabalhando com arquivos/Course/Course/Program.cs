8ddb505c4140c2ec884327a3e0aa70fb680ef923
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-10-02 18:59:01 -0300

    Trabalhando com arquivos - File, FileInfo e IOException

---
+++ Trabalhando com arquivos/Course/Course/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            string sourcePath = @"C:\Users\Leonardo\Documents\Leonardo\Git\C#\C# Completo 2018 - Programação Orientada a Objetos\Trabalhando com arquivos\file1.txt";
+            string targetPath = @"C:\Users\Leonardo\Documents\Leonardo\Git\C#\C# Completo 2018 - Programação Orientada a Objetos\Trabalhando com arquivos\file2.txt";
+
+            try {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines) {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
