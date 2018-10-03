02554e78328d7ca43c29f9d1560c88a00835cf2c
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-10-03 17:55:08 -0300

    Trabalhando com arquivos - Exercício de fixação

---
+++ Trabalhando com arquivos/Exercícios/fix/fix/Entidades/Produto.cs
@@ -0,0 +1,19 @@
+﻿namespace fix.Entidades {
+    class Produto {
+
+        public string Nome { get; private set; }
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto(string nome, double preco, int quantidade) {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Quantidade = quantidade;
+        }
+
+        public double ValorTotal() {
+            return Preco * Quantidade;
+        }
+
+    }
+}
---
+++ Trabalhando com arquivos/Exercícios/fix/fix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+using fix.Entidades;
+
+namespace fix {
+    class Program {
+        static void Main(string[] args) {
+
+            string caminhoOrigem = @"C:\Users\Leonardo\Documents\Leonardo\Git\C#\C# Completo 2018 - Programação Orientada a Objetos\Trabalhando com arquivos\Exercícios\origem\arquivo.csv";
+            string caminhoDestino = @"C:\Users\Leonardo\Documents\Leonardo\Git\C#\C# Completo 2018 - Programação Orientada a Objetos\Trabalhando com arquivos\Exercícios\origem\";
+
+            List<Produto> produtos = new List<Produto>();
+
+            try {
+                using (StreamReader sr = File.OpenText(caminhoOrigem)) {
+                    while (!sr.EndOfStream) {
+                        string[] vet = sr.ReadLine().Split(',');
+
+                        string nome = vet[0];
+                        double preco = double.Parse(vet[1], CultureInfo.InvariantCulture);
+                        int quantidade = int.Parse(vet[2]);
+
+                        Produto produto = new Produto(nome, preco, quantidade);
+
+                        produtos.Add(produto);
+                    }
+
+                    Directory.CreateDirectory(caminhoDestino + @"\out");
+
+                    using (StreamWriter sw = File.AppendText(caminhoDestino + @"\out\summary.csv")) {
+                        foreach (Produto produto in produtos) {
+                            sw.WriteLine(produto.Nome + "," + produto.ValorTotal().ToString("F2", CultureInfo.InvariantCulture));
+                        }
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("Um erro ocorreu");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
