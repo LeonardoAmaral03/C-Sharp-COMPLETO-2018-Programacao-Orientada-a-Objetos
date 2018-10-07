2bac734102d99dabcf40b6fdf97871e8d08ed6cf
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-10-07 18:16:42 -0300

    Expressões lambda, delegates, LINQ - Programação Funcional e Cálculo Lambda

---
+++ Expressões lambda, delegates, LINQ/course2/course2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+
+        public static int globalValue = 3;
+
+        static void Main(string[] args) {
+
+            int[] vect = new int[] { 3, 4, 5 };
+            ChangeOddValues(vect);
+            Console.WriteLine(string.Join(" ", vect));
+        }
+
+        // Exemplo de função que não é referencialmene transparente
+        public static void ChangeOddValues(int[] numbers) {
+
+            for (int i = 0; i < numbers.Length; i++) {
+                if (numbers[i] % 2 != 0) {
+                    numbers[i] += globalValue;
+                }
+            }
+        }
+    }
+}
