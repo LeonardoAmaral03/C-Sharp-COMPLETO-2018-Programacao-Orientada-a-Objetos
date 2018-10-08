73a815b00639eebded62cf4da2fec678a940ebed
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-10-08 18:57:42 -0300

    Expressões lambda, delegates, LINQ - Action (Exemplo com ForEach)

---
+++ Expressões lambda, delegates, LINQ/course6/course6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using course6.Entities;
+
+namespace course6 {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.ForEach(p => { p.Price += p.Price * 0.1; }); // função lambda inline
+
+            //Action<Product> act = p => { p.Price += p.Price * 0.1; }; // {} significa que não haverá retorno da função lambda (void)
+            //list.ForEach(act);
+
+            //Action<Product> act = UpdatePrice; // Criando um objeto do tipo delegate Action<>
+            //list.ForEach(act);
+
+            //list.ForEach(UpdatePrice);
+
+            foreach (Product p in list) {
+                Console.WriteLine(p);
+            }
+
+        }
+
+        static void UpdatePrice(Product p) {
+            p.Price += p.Price * 0.1;
+        }
+
+    }
+}
