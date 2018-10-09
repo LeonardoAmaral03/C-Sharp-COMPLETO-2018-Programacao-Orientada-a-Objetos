6816a10a14b5f32a5793c57d5bae21b333827b5f
Leonardo Amaral da Rocha <leoamaral_contatos@outlook.com>
2018-10-09 20:31:48 -0300

    Expressões lambda, delegates, LINQ - Demo - LINQ com Lambda - PARTE 1

---
+++ Expressões lambda, delegates, LINQ/course9/course9/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace course9.Entities {
+    class Category {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
+
+    }
+}
